725a25db8fe0e123eaafa497efa35ac5bf036b04
Python1965 <python65@rambler.ru>
2024-10-09 23:25:40 +0300

    Add files via upload

--- Module_9_3.py
+++ Module_9_3.py
@@ -23,9 +23,9 @@ def start():
     first_result = ((abs(len(x) - len(y))) for x, y in zip(first, second) if len(x) != len(y))
     print(list(first_result))
 
-    # second_result = (first[i] == second[i] for i in range(len(first)))
-    # print(list(second_result))
-    
+    second_result = (first[i] == second[i] for i in range(len(first)))
+    print(list(second_result))
+
 
 if __name__ == '__main__':
     start()
